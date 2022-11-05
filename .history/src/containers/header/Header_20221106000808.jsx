45e9fa9a504086a61b637b6caaffed487e5077e4
alexbanilean <alexandru.banilean@gmail.com>
2022-11-06 01:31:17 +0200

    half homepage

---
+++ .history/src/containers/header/Header_20221106000808.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='ckmint__header section__padding' id='home'>
+      <div className='ckmint__header-content'>
+        <h1 className='gradient__text'>Create your own cryptocurrency with no code required</h1>
+        <p> Our easy-to-use crypto generator allows you to make your own custom token on the Ethereum blockchain without needing any programming experience.</p>
+        <div className='ckmint__header-content-input'>
+          <button type='button'>Get started</button>
+          <button type='button'>Learn more</button>
+        </div>
+        <div className=''
+      </div>
+    </div>
+  )
+}
+
+export default Header
